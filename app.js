68b5939f2396121bc4864b158a0f34c7e856df9b
M Hughes <mhughes2011@gmail.com>
2021-04-22 22:14:09 -0700

    added the remove btn to list items

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ form.addEventListener('submit', (e) => {
   checkbox.type = 'checkbox';//This sets the input type to checkbox.
   label.appendChild(checkbox);//This adds the checkbox to the label element.
   li.appendChild(label);//This appends the label (and checkbox) to the li
+  
+  let button = document.createElement('button');//This creates the remove btn.
+  button.textContent = 'remove';//This sets the btn's text to remove.
+  li.appendChild(button);//This adds a remove btn to each list item.
+  
   ul.appendChild(li);//This adds the li to the blank ul.
 });
 
-//This is listening for the change of the confirmed checkbox and adding the classname to the element.  This involves event bubbling.  The event listener is on the ul because if the ul is triggered the child elements will be triggered as well.
+//This is listening for the change of the confirmed checkbox and adding the classname to the element.  This involves event bubbling.  The event listener is on the ul because if the child elements are triggered, the event bubbles up to the parent elements in this case the ul.
 ul.addEventListener('change', (e) => {
   let checkbox = event.target;//This grabs the target of the event (the confirmed checkbox)
   let checked = checkbox.checked;//This stores the value of the checked confirmed box.
@@ -29,3 +34,20 @@ ul.addEventListener('change', (e) => {
     listItem.className = '';
   }
 });
+
+ul.addEventListener('click', (e) => {
+  //This if statement makes sure that the event is only triggered when you click a button.
+  if(e.target.tagName === 'BUTTON') {
+    let li = e.target.parentNode;//This targets the parent node of the remove btn that is clicked, which is the li that you want to delete.
+    let ul = li.parentNode;//This grabs the ul that the list item that wants to be deleted is in.
+    ul.removeChild(li);
+  }
+});
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
